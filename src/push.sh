4a6196c6b9a1d5e176de470b418304e7ed44dcd7
Simon Constans <simonc@linux.com>
2016-10-15 17:57:32 +0200

    Travis push

--- src/push.sh
+++ src/push.sh
@@ -1,5 +1,8 @@
 if ! git diff-index --quiet HEAD --; then
     echo "modification des images"
+    git add build/img
+    git ci -m "Resize picture"
+    git push
 else
     echo "pas de changement"
 fi
