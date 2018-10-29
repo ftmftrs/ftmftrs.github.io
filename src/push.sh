8a67b85923ed1e1c142a90e2d70fc42d951072c9
Simon Constans <simonc@linux.com>
2018-10-29 12:04:34 +0100

    change les droits du scripts de mise à jour des photos

--- src/push.sh
+++ src/push.sh
@@ -1,8 +1,10 @@
 if ! git diff-index --quiet HEAD --; then
     echo "modification des images"
-    git add build/img
-    git ci -m "Resize picture"
-    git push
+    git add dist/img
+    git commit -m "Resize picture"
+    git remote rm origin
+    git remote add origin https://kosssi:$GITHUB_TOKEN@github.com/ftmftrs/ftmftrs.github.io.git
+    git push origin HEAD:master
 else
     echo "pas de changement"
 fi
